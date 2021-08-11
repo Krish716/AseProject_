727b84aca7e640e45b9f17297c21464128232ad0
Krish716 <saikrishnaadusumilli@gmail.com>
2021-08-10 20:46:50 -0400

    update App.js

--- project/App.js
+++ project/App.js
@@ -5,22 +5,22 @@ import { createStackNavigator } from '@react-navigation/stack';
 import * as Font from 'expo-font';
 import HomeScreen from "./navigation/HomeScreen";
 import CameraScreen from "./navigation/CameraScreen";
-import Location from "./navigation/Location";
 import SigInScreen from "./navigation/SigInScreen";
 import AdminScreen from "./navigation/AdminScreen";
 import Camera from "./navigation/Camera";
 import ReceivedRequests from "./navigation/ReceivedRequests";
 import Request from "./navigation/Request";
-import OnBoarding from './navigation/OnBoarding';
 import Reqscreen from './navigation/Reqscreen';
-import ReceivedRequestsScreen from './navigation/ReceivedRequestsScreen';
-import Notifications from './navigation/Notifications';
-
+import Notifications from './navigation/Notifications'
+import ReceivedRequestsScreen from "./navigation/ReceivedRequestsScreen";
+import RejectedRequestScreen from "./navigation/RejectedRequestScreen";
+import AcceptedRequest from "./navigation/AcceptedRequest";
+import RejectedRequest from "./navigation/RejectedRequest";
 
 import * as firebase from "firebase";
 
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ var firebaseConfig = {
   apiKey: "AIzaSyBVZmfOXFASWBmEandPj_N-778nQSWxmTk",
   authDomain: "smart-5f720.firebaseapp.com",
   projectId: "smart-5f720",
@@ -29,9 +29,12 @@ var firebaseConfig = {
   appId: "1:937200982084:web:38f8553f28e854b2c24411",
   measurementId: "G-KW10WJG6HN"
 };
-
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+
 
 
 const Stack = createStackNavigator();
@@ -66,14 +69,15 @@ export default class App extends React.Component {
       <Stack.Screen name ="SignIn" component={SigInScreen} />
       <Stack.Screen name ="Admin" component={AdminScreen} />
       <Stack.Screen name ="Camera" component={Camera} />
-      <Stack.Screen name ="Location" component={Location} />
-      <Stack.Screen name ="Received" component={ReceivedRequests} />
+      <Stack.Screen name ="ReceivedRequests" component={ReceivedRequests} />
       <Stack.Screen name ="Request" component={Request} />
-      <Stack.Screen name ="OnBoarding" component={OnBoarding} />
       <Stack.Screen name ="Reqscreen" component={Reqscreen} />
       <Stack.Screen name ="Notifications" component={Notifications} />
-      <Stack.Screen name ="ReceivedRequestsScreen" component={ReceivedRequestsScreen} />
-      </Stack.Navigator>
+      <Stack.Screen name ="ReceivedRequestsScreen" component={ReceivedRequestsScreen} /> 
+      <Stack.Screen name ="RejectedRequestScreen" component={RejectedRequestScreen} /> 
+      <Stack.Screen name ="AcceptedRequest" component={AcceptedRequest} /> 
+      <Stack.Screen name ="RejectedRequest" component={RejectedRequest} /> 
+    </Stack.Navigator>
   </NavigationContainer>
   );
  }
@@ -82,10 +86,10 @@ export default class App extends React.Component {
  }
   }
 }
-    
-    
-  const styles = StyleSheet.create({
-    container: {
+
+
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
     backgroundColor: 'blue',
     alignItems: 'center',
